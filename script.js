cd2302d68964de2554391b1bc06056033c6c9faf
shanky202 <shankypachore@gmail.com>
2020-08-31 19:38:57 +0000

    added imgaes and all the styling and functionality to the projects

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("testing");
+const { body } = document;
+
+function changeBackground(number) {
+    // If background is alredy exists
+    let previousBackground ;
+    if (body.className) {
+        previousBackground = body.className;
+    }
+    // Reset CSS class for body
+    body.className = '';
+    switch (number){
+        case '1' :
+            return (previousBackground === 'background-1' ? false : body.className = 'background-1')
+        case '2' :
+            return (previousBackground === 'background-2' ? false : body.className = 'background-2')
+        case '3' :
+            return (previousBackground === 'background-3' ? false : body.className = 'background-3')
+    }
+}
